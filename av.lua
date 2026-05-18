9c2eca2225c72823af426831fd0987b040bc56ad
Lunny Nymph <99461156+Lunny-Nymph@users.noreply.github.com>
2026-05-18 03:16:58 -0300

    Update av.lua

--- av.lua
+++ av.lua
@@ -1,1 +1,58 @@
+return {
 
+	[7623357110] = {
+		BodyTypeScale = 0,
+		ProportionScale = 0,
+		HeightScale = 1,
+		WidthScale = 1,
+		HeadScale = 1,
+		DepthScale = 1,
+
+		Shirt  = 121132030626744,
+		Pants  = 112334704915099,
+		Torso  = 48474356,
+
+		FaceAccessory  = "96612178366749,13399898495",
+		HairAccessory  = "12850630072,116788699891272,17274655265,10323360626",
+		HatAccessory   = "15772501759",
+		NeckAccessory  = "17010945991,105280197073759",
+		WaistAccessory = "13632154889",
+
+		HeadColor     = Color3.fromRGB(249, 249, 249),
+		TorsoColor    = Color3.fromRGB(249, 249, 249),
+		LeftArmColor  = Color3.fromRGB(249, 249, 249),
+		RightArmColor = Color3.fromRGB(249, 249, 249),
+		LeftLegColor  = Color3.fromRGB(249, 249, 249),
+		RightLegColor = Color3.fromRGB(249, 249, 249),
+	},
+	
+	[13] = {
+		BodyTypeScale = 0,
+		ProportionScale = 0,
+		HeightScale = 1,
+		WidthScale = 1,
+		HeadScale = 1,
+		DepthScale = 1,
+		
+		Head = 87337809867327,
+		Torso = 48474356,
+		Shirt = 6903572692,
+		Pants = 7108200232,
+		
+		ClimbAnimation = 973773170,
+		FallAnimation = 973768058,
+		IdleAnimation = 973771666,
+		JumpAnimation = 973770652,
+		RunAnimation = 973766674,
+		SwimAnimation = 973772659,
+		WalkAnimation = 973767371,
+		
+		HatAccessory = "18418395303,109122262706835",
+		HeadColor = Color3.fromRGB(215, 197, 154),
+		TorsoColor = Color3.fromRGB(215, 197, 154),
+		LeftArmColor = Color3.fromRGB(215, 197, 154),
+		RightArmColor = Color3.fromRGB(215, 197, 154),
+		LeftLegColor = Color3.fromRGB(215, 197, 154),
+		RightLegColor = Color3.fromRGB(215, 197, 154),
+	}
+}
